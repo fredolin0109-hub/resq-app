a2d476df154063aad99ee0b385d715de8e0343cc
praneshramaraj <praneshramaraj2747@gmail.com>
2026-09-07 09:17:27 +0530

    chore(platform): add platform configs and fix CardThemeData in theme.dart

--- mobile/lib/app/theme/theme.dart
+++ mobile/lib/app/theme/theme.dart
@@ -26,7 +26,7 @@ class AppTheme {
         elevation: 0,
         centerTitle: true,
       ),
-      cardTheme: CardTheme(
+      cardTheme: CardThemeData(
         color: surface,
         elevation: 0,
         shape: RoundedRectangleBorder(
